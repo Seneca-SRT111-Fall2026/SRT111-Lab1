15253355b63258ab16677e887ad9bb25b31de74e
Tiayyba Riaz <121324609+tiayyba@users.noreply.github.com>
2025-08-12 21:55:36 -0400

    Update lab1b.py

--- lab1b.py
+++ lab1b.py
@@ -9,7 +9,7 @@
 # TO-DO 1:
 #	Create a variable called "num1", take its value from user.
 #	Create another variable called "num2" and take its value from user. 
-# Convert the values to integers using int() function
+# Convert the values to float using float() function
 
 # TO-DO 2:
 # Perform all arithmetic oeprations as outlined in the description in README.md file, and print in the required format.
