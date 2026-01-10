e498ed756a2c1c5390141677747f200336527dbf
Tiayyba Riaz <121324609+tiayyba@users.noreply.github.com>
2026-01-09 21:39:19 -0500

    Update lab1c.py

--- lab1c.py
+++ lab1c.py
@@ -7,8 +7,3 @@
 # Purpose: Use string methods and f-string formating.
 # Usage: ./lab1c.py
 
-#TO-DO 1:
-# import math module.
-# Create a variable called 'radius' and take its value form user.
-# Convert the variable to float using float()
-# use the contant pi form math module and compute the area of the circle using the variable 'radius'
